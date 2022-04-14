efafeaed1ede955c1c09040fa03926ca2453918c
blacklisss <rmjv@mail.ru>
2022-04-14 16:13:45 +0300

    Fix

--- internal/client/client.go
+++ internal/client/client.go
@@ -6,6 +6,7 @@ import (
 	"net/http"
 	"site_monitoring/internal/model"
 	"site_monitoring/internal/notification"
+	"strings"
 	"sync"
 	"time"
 
@@ -48,91 +49,21 @@ func (c *Client) GetHeaders(ctx context.Context, url string, wg *sync.WaitGroup)
 			c.Logger.Fatalln("cannot create request")
 		}
 
-	L:
+		ctx2, cancel2 := context.WithTimeout(ctx, 30*time.Second)
+		t := time.Now()
+		c.Do(ctx2, cancel2, url, resp, req, t)
+
 		for {
 			select {
 			case <-ctx.Done():
 				c.Logger.Warnln("exiting...")
-				break L
-			case t := <-ticker.C:
-				ctx, cancel := context.WithTimeout(ctx, 30*time.Second)
-
-				req = req.WithContext(ctx)
-
-				c.Logger.Infof("Checking %s at %s\n", url, t)
-
-				res, err := c.C.Do(req)
-				if err != nil {
-					cancel()
-					c.Logger.Errorln("cannot do request")
-					resp.ResponseCode = 503
-					resp.ContentLength = 0
-				} else {
-					resp.ResponseCode = res.StatusCode
-					/*b, err := io.Copy(io.Discard, res.Body)
-					if err != nil {
-						cancel()
-						c.Logger.Errorln("cannot get content-length")
-						continue
-					}
-					err = res.Body.Close()
-					if err != nil {
-						cancel()
-						c.Logger.Fatalln("cannot close resp.Body")
-					}
-					resp.ContentLength = b*/
-				}
-
-				c.Lock()
-				m[url] = resp
-				c.Unlock()
-
-				ok, err := c.CheckRespStatus(url)
-				if err != nil {
-					c.Logger.Errorln(err)
-					continue
-				}
-
-				if !ok && resp.OldResponseCode != resp.ResponseCode {
-					if res != nil {
-						resp.OldResponseCode = res.StatusCode
-					} else {
-						resp.OldResponseCode = resp.ResponseCode
-					}
-
-					err = c.Notificator.SendMessage(fmt.Sprint("Server status not ", http.StatusOK, " in url: ", url,
-						" at ", time.Now().Format("2006-01-02 15:04:05")))
-					if err != nil {
-						logrus.Errorln("cannot send tg message about server status")
-					}
-				} else if resp.OldResponseCode != resp.ResponseCode {
-					resp.OldResponseCode = res.StatusCode
-					err = c.Notificator.SendMessage(fmt.Sprint("Server started up in url: ", url,
-						" at ", time.Now().Format("2006-01-02 15:04:05")))
-					if err != nil {
-						logrus.Errorln("cannot send tg message about server status")
-					}
-				} /*else if ok {
-					ok, err = CheckRespContentLength(url)
-					if err != nil {
-						cancel()
-						c.Logger.Errorln(err)
-						continue
-					}
-
-					if !ok {
-						err = c.Notificator.SendMessage(fmt.Sprint("Content-Length was changed in url: ", url,
-							" at ", time.Now().Format("2006-01-02 15:04:05")))
-						if err != nil {
-							logrus.Errorln("cannot send tg message about content-length")
-						}
-					}
-				}*/
-				c.Lock()
-				m[url] = resp
-				c.Unlock()
+				return
+			case t = <-ticker.C:
+				ctx2, cancel2 = context.WithTimeout(ctx, 30*time.Second)
+				c.Do(ctx2, cancel2, url, resp, req, t)
 			}
 		}
+
 	}()
 
 	return nil
@@ -174,4 +105,85 @@ func (c *Client) CheckRespContentLength(url string) (bool, error) {
 	c.Unlock()
 
 	return ret, nil
+}
+
+func (c *Client) Do(ctx context.Context, cancel context.CancelFunc, url string, resp *model.Resp, req *http.Request, t time.Time) {
+	req = req.WithContext(ctx)
+
+	c.Logger.Infof("Checking %s at %s\n", url, t)
+
+	res, err := c.C.Do(req)
+	if err != nil {
+		c.Logger.Errorln("cannot do request: ", err.Error())
+		if strings.Contains(err.Error(), ": context canceled") {
+			cancel()
+			return
+		}
+		resp.ResponseCode = 503
+		resp.ContentLength = 0
+	} else {
+		resp.ResponseCode = res.StatusCode
+		/*b, err := io.Copy(io.Discard, res.Body)
+		if err != nil {
+			cancel()
+			c.Logger.Errorln("cannot get content-length")
+			continue
+		}
+		err = res.Body.Close()
+		if err != nil {
+			cancel()
+			c.Logger.Fatalln("cannot close resp.Body")
+		}
+		resp.ContentLength = b*/
+	}
+
+	c.Lock()
+	m[url] = resp
+	c.Unlock()
+
+	ok, err := c.CheckRespStatus(url)
+	if err != nil {
+		cancel()
+		c.Logger.Errorln(err)
+		return
+	}
+
+	if !ok && resp.OldResponseCode != resp.ResponseCode {
+		if res != nil {
+			resp.OldResponseCode = res.StatusCode
+		} else {
+			resp.OldResponseCode = resp.ResponseCode
+		}
+
+		err = c.Notificator.SendMessage(fmt.Sprint("Server status not ", http.StatusOK, " in url: ", url,
+			" at ", time.Now().Format("2006-01-02 15:04:05")))
+		if err != nil {
+			logrus.Errorln("cannot send tg message about server status")
+		}
+	} else if resp.OldResponseCode != resp.ResponseCode {
+		resp.OldResponseCode = res.StatusCode
+		err = c.Notificator.SendMessage(fmt.Sprint("Server started up in url: ", url,
+			" at ", time.Now().Format("2006-01-02 15:04:05")))
+		if err != nil {
+			logrus.Errorln("cannot send tg message about server status")
+		}
+	} /*else if ok {
+		ok, err = CheckRespContentLength(url)
+		if err != nil {
+			cancel()
+			c.Logger.Errorln(err)
+			continue
+		}
+
+		if !ok {
+			err = c.Notificator.SendMessage(fmt.Sprint("Content-Length was changed in url: ", url,
+				" at ", time.Now().Format("2006-01-02 15:04:05")))
+			if err != nil {
+				logrus.Errorln("cannot send tg message about content-length")
+			}
+		}
+	}*/
+	c.Lock()
+	m[url] = resp
+	c.Unlock()
 }
